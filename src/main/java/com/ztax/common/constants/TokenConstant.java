71a6299ee0d071cdca02e1853cf1d35d500b4e5f
SUN <15144639564@163.com>
2022-03-24 11:53:37 +0800

    增加常量

--- src/main/java/com/ztax/common/constants/TokenConstant.java
+++ src/main/java/com/ztax/common/constants/TokenConstant.java
@@ -3,4 +3,7 @@ package com.ztax.common.constants;
 public class TokenConstant {
     public static final String TOKEN_NAME_IN_HEADER = "Authorization";
     public static final String TOKEN_PREFIX = "ZtaxToken ";
+    public static final String TOKEN_KEY_JTI = "jti ";
+    public static final String TOKEN_KEY_EXP = "exp ";
+
 }
