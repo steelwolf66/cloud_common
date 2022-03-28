f4565754e6b5d280b821a10f6d26e986498b6eb8
SUN <15144639564@163.com>
2022-03-28 18:09:16 +0800

    修改依赖 修改readMe

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # common
-common beans or utils for local
+## 底层依赖包
+- 包括SpringBoot SpringCloud Hutool JWT解析工具等
+- 该依赖包只能被常规Servlet容器使用，不可作用于WebFlux等其他web容器的应用（当前架构中的Spring Cloud Gateway不可依赖此包）
--- src/main/java/com/ztax/common/constants/TokenConstant.java
+++ src/main/java/com/ztax/common/constants/TokenConstant.java
@@ -2,8 +2,8 @@ package com.ztax.common.constants;
 
 public class TokenConstant {
     public static final String TOKEN_NAME_IN_HEADER = "Authorization";
-    public static final String TOKEN_PREFIX = "ZtaxToken ";
-    public static final String TOKEN_KEY_JTI = "jti ";
-    public static final String TOKEN_KEY_EXP = "exp ";
+    public static final String TOKEN_PREFIX = "ZtaxToken";
+    public static final String TOKEN_KEY_JTI = "jti";
+    public static final String TOKEN_KEY_EXP = "exp";
 
 }
