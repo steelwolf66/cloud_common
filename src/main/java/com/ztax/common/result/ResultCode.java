aac7b9845a7c7b34b43992b8a09be3b87eec5d2b
SUN <15144639564@163.com>
2022-04-06 11:49:10 +0800

    - 增加 删除自适应切面 - 修改成功的错误码编码

--- src/main/java/com/ztax/common/result/ResultCode.java
+++ src/main/java/com/ztax/common/result/ResultCode.java
@@ -12,7 +12,7 @@ import java.io.Serializable;
 @NoArgsConstructor
 public enum ResultCode implements IResultCode, Serializable {
 
-    SUCCESS("00000", "一切ok"),
+    SUCCESS("00000", "成功"),
 
     USER_ERROR("A0001", "用户端错误"),
     USER_LOGIN_ERROR("A0200", "用户登录异常"),
