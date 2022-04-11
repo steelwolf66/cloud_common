589fff298738e4e62d750b832fc425e6811e350e
SUN <15144639564@163.com>
2022-04-11 16:12:15 +0800

    修改异常处理http响应码

--- src/main/java/com/ztax/common/exception/GlobalExceptionHandler.java
+++ src/main/java/com/ztax/common/exception/GlobalExceptionHandler.java
@@ -2,9 +2,12 @@ package com.ztax.common.exception;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.ztax.common.result.Result;
+import com.ztax.common.utils.HttpUtils;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import javax.servlet.http.HttpServletResponse;
 
 /**
  * 全局系统异常处理
@@ -16,6 +19,7 @@ public class GlobalExceptionHandler {
     @ExceptionHandler(IllegalArgumentException.class)
     public Result handleIllegalArgumentException(IllegalArgumentException e) {
         log.error("非法参数异常，异常原因：{}",e.getMessage(),e);
+        HttpUtils.getResponse().setStatus(HttpServletResponse.SC_BAD_REQUEST);
         return Result.failed(e.getMessage());
     }
 
@@ -23,6 +27,7 @@ public class GlobalExceptionHandler {
     @ExceptionHandler(JsonProcessingException.class)
     public Result handleJsonProcessingException(JsonProcessingException e) {
         log.error("Json转换异常，异常原因：{}",e.getMessage(),e);
+        HttpUtils.getResponse().setStatus(HttpServletResponse.SC_BAD_REQUEST);
         return Result.failed(e.getMessage());
     }
 
@@ -32,12 +37,14 @@ public class GlobalExceptionHandler {
         if (e.getResultCode() != null) {
             return Result.failed(e.getResultCode());
         }
+        HttpUtils.getResponse().setStatus(HttpServletResponse.SC_BAD_REQUEST);
         return Result.failed(e.getMessage());
     }
 
     @ExceptionHandler(Exception.class)
     public Result handleException(Exception e) {
         log.error("未知异常，异常原因：{}",e.getMessage(),e);
+        HttpUtils.getResponse().setStatus(HttpServletResponse.SC_BAD_REQUEST);
         return Result.failed(e.getMessage());
     }
 }
